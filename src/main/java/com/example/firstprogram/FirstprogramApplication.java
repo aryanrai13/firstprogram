22b27fcab023634ac32528efddf6a70c5075b37d
Deepak Rai <deepak.rai@ad.infosys.com>
2019-10-21 19:01:42 +0200

    Initial commit

---
+++ src/main/java/com/example/firstprogram/FirstprogramApplication.java
@@ -0,0 +1,16 @@
+package com.example.firstprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprogramApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstprogramApplication.class, args);
+
+		System.out.println("Now the output is visible!");
+	}
+
+}
